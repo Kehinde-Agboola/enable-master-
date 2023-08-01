9e7388321dc5a4c7da54f0e9a97a94e032fe18dd
kehindeoluseyi18 <kehindeoluseyi18@gmail.com>
2023-08-01 14:59:15 +0100

    glogos

--- src/components/UI/Home/Partners/index.tsx
+++ src/components/UI/Home/Partners/index.tsx
@@ -4,7 +4,9 @@ import tw from "twin.macro";
 import { partnerLogos } from "../../../../constants";
 import { Container } from "../../../shared";
 const Glogos = styled.div`
-  ${tw`-mt-5 grid grid-cols-4 place-items-center gap-5 md:gap-10`}
+  ${tw`-mt-5 grid grid-cols-4 place-items-center gap-5 md:gap-10
+  w-[100%] h-[100%]
+  `}
 `;
 const IMG = styled.img`
    {
@@ -18,11 +20,11 @@ export const Partners = () => {
         <Marquee pauseOnHover autoFill>
           <div>
             <Glogos>
-              <IMG>
-                {partnerLogos?.map((img, index) => {
-                  return <img src={`${img}`} alt="partner-logos" key={index} />;
-                })}
-              </IMG>
+              {/* <IMG> */}
+              {partnerLogos?.map((img, index) => {
+                return <img src={`${img}`} alt="partner-logos" key={index} />;
+              })}
+              {/* </IMG> */}
             </Glogos>
           </div>
         </Marquee>
