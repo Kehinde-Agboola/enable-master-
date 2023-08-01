4948fa2f8a4da6c4956b51aba221043d23bba4e3
kehindeoluseyi18 <kehindeoluseyi18@gmail.com>
2023-08-01 14:56:18 +0100

    partners

--- src/components/UI/Home/Partners/index.tsx
+++ src/components/UI/Home/Partners/index.tsx
@@ -6,6 +6,11 @@ import { Container } from "../../../shared";
 const Glogos = styled.div`
   ${tw`-mt-5 grid grid-cols-4 place-items-center gap-5 md:gap-10`}
 `;
+const IMG = styled.img`
+   {
+    ${tw`w-[100%] h-[100%]`}
+  }
+`;
 export const Partners = () => {
   return (
     <div>
@@ -13,9 +18,11 @@ export const Partners = () => {
         <Marquee pauseOnHover autoFill>
           <div>
             <Glogos>
-              {partnerLogos?.map((img, index) => {
-                return <img src={`${img}`} alt="partner-logos" key={index} />;
-              })}
+              <IMG>
+                {partnerLogos?.map((img, index) => {
+                  return <img src={`${img}`} alt="partner-logos" key={index} />;
+                })}
+              </IMG>
             </Glogos>
           </div>
         </Marquee>
